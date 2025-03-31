7384f3943f76b71ec6aec28ff1c69eccb3c002be
sznorbi48 <norbertszabo834@gmail.com>
2025-03-31 10:35:03 +0200

    Add files via upload

---
+++ kod.ino
@@ -0,0 +1,18 @@
+const int buttonPin = 2;  // a nyomógomb a 2-es digitális bemenetre van kötve
+int buttonState = 0;      // változó a nyomógomb állapotának tárolására
+ 
+void setup() {
+Serial.begin(9600);     // soros kommunikáció indítása 9600 baud sebességgel
+pinMode(buttonPin, INPUT_PULLUP);  // a 2-es bemenetet pull-up ellenállással konfiguráljuk
+}
+ 
+void loop() {
+buttonState = digitalRead(buttonPin);  // olvassuk a nyomógomb állapotát
+ 
+if (buttonState == LOW) {  // ha a gomb lenyomva (LOW)
+Serial.println("Lenyomva");
+} else {  // ha a gomb felengedve (HIGH)
+Serial.println("Felengedve");
+}
+delay(100);  // kis késleltetés a stabilabb működés érdekében
+}
